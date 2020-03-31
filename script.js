a0e23eb16a0b23468f284c0a553d04a2e075fec1
ClaudioDroid <claudio.v.lima2@gmail.com>
2020-03-30 21:35:15 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+document
+			.querySelector(".fat")
+			.addEventListener("click", function(){
+				document
+                    .querySelector("#footer")
+                    .classList
+                    .toggle("hide")
+            })
+
+
+
